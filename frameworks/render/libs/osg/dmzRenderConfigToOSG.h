f2242027a17ea804259f9412b07d69f719a527c5
U-MAXIMUS-71A7EE4\Maximus <Maximus@maximus-71a7ee4.(none)>
2008-12-08 13:31:32 -0800

    Moved RenderToConfig funtion definiton from header to cpp

--- frameworks/render/libs/osg/dmzRenderConfigToOSG.h
+++ frameworks/render/libs/osg/dmzRenderConfigToOSG.h
@@ -1,7 +1,7 @@
 #ifndef DMZ_RENDER_CONFIG_TO_OSG_DOT_H
 #define DMZ_RENDER_CONFIG_TO_OSG_DOT_H
 
-#include <dmzRuntimeConfig.h>
+#include <dmzRenderUtilOSGExport.h>
 #include <dmzRuntimeConfigToTypesBase.h>
 #include <dmzTypesBase.h>
 #include <osg/Vec4>
@@ -10,10 +10,10 @@ namespace dmz {
 
    class Config;
 
-   osg::Vec4 config_to_osg_vec4_color ( const Config &Source);
-   osg::Vec4 config_to_osg_vec4_color ( const Config &Source, const osg::Vec4 &Value);
-   osg::Vec4 config_to_osg_vec4_color ( const String &Name, const Config &Source);
-   osg::Vec4 config_to_osg_vec4_color ( 
+   osg::Vec4 config_to_osg_vec4_color (const Config &Source);
+   osg::Vec4 config_to_osg_vec4_color (const Config &Source, const osg::Vec4 &Value);
+   osg::Vec4 config_to_osg_vec4_color (const String &Name, const Config &Source);
+   DMZ_RENDER_UTIL_OSG_LINK_SYMBOL osg::Vec4 config_to_osg_vec4_color ( 
       const String &Name, 
       const Config &Source, 
       const osg::Vec4 &DefaultValue);
@@ -42,29 +42,6 @@ dmz::config_to_osg_vec4_color (
 
    const osg::Vec4 Value;
    return config_to_osg_vec4_color ("", Source, Value);
-}
-
-osg::Vec4
-dmz::config_to_osg_vec4_color (
-      const String &Name,
-      const Config &Source,
-      const osg::Vec4 &DefaultValue) {
-
-   Config cd;
-
-   if (Name) {
-
-      Source.lookup_config (Name, cd);
-   }
-   else { cd = Source; }
-
-   const osg::Vec4 Result (
-      config_to_float32 ("r", cd, DefaultValue.x ()),
-      config_to_float32 ("g", cd, DefaultValue.y ()),
-      config_to_float32 ("b", cd, DefaultValue.z ()),
-      DefaultValue.w ());
-
-   return Result;
 }
 
 #endif // DMZ_RENDER_CONFIG_TO_OSG_DOT_H
